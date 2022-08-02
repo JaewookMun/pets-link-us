9065e8357476d3785bcc0dac98e54d93c8c023e6
JaewookMun <mun1103.dev@gmail.com>
2022-08-03 00:39:14 +0900

    commit petslinkus

---
+++ src/main/java/prj/server/petslinkus/PetslinkusApplication.java
@@ -0,0 +1,13 @@
+package prj.server.petslinkus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetslinkusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetslinkusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/prj/server/petslinkus/PetslinkusApplicationTests.java
@@ -0,0 +1,13 @@
+package prj.server.petslinkus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetslinkusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
